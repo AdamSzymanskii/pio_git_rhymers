2964c13128a2bfbc6a826766f94f61c28b54c565
Adam Szymański <245939@edu.p.lodz.pl>
2024-04-08 13:41:54 +0200

    2. Zmaiana formatowania kodu.

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,15 +1,16 @@
 package edu.kis.vh.nursery;
 
-public class HanoiRhymer extends defaultCountingOutRhymer {
+public class HanoiRhymer extends defaultCountingOutRhymer
+{
 
-int totalRejected = 0;
-
-    public int reportRejected() {
+    int totalRejected = 0;
+    public int reportRejected()
+    {
         return totalRejected;
     }
-
-    public void countIn(int in) {
-    if (!callCheck() && in > peekaboo())
+    public void countIn(int in)
+    {
+        if(!callCheck() && in > peekaboo())
             totalRejected++;
             else
                 super.countIn(in);
